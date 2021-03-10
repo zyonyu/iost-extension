539f8c39c3b3c653fc88d97dd107b81e0f135969
mzw347 <vv347@qq.com>
2021-03-10 13:55:32 +0800

    add icon for DON

--- src/constants/icon.js
+++ src/constants/icon.js
@@ -3,7 +3,7 @@ const iconSrc = {
   husd: '/static/images/icon-token-husd.png',
   lol: '/static/images/icon-token-lol.png',
   abct: '/static/images/icon-token-abct.svg',
-  bait:  '/static/images/icon-token-bait.png',
+  bait: '/static/images/icon-token-bait.png',
   idt: '/static/images/icon-token-idt.png',
   ifry: '/static/images/icon-token-ifry.png',
   iplay: '/static/images/icon-token-iplay.svg',
@@ -12,6 +12,7 @@ const iconSrc = {
   tix: '/static/images/icon-token-tix.png',
   vost: '/static/images/icon-token-vost.svg',
   win: '/static/images/icon-token-win.png',
+  don: '/static/images/icon-token-don.png',
   default: '/static/images/icon-token-default.png',
 }
 
